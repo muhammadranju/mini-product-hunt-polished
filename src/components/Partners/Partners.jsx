ac0d5fb3669dc5d006a5fc8ae832012aa2cf93c8
Muhammad Ranju <mdranju23@gmail.com>
2025-02-18 10:09:05 +0600

    Add Partners component to highlight collaborations with leading brands

---
+++ src/components/Partners/Partners.jsx
@@ -0,0 +1,49 @@
+const Partners = () => {
+  return (
+    <div className="py-16 bg-gray-50">
+      <div className="w-11/12 xl:w-2/3 lg:w-2/3 md:w-2/3 mx-auto">
+        <div className=" sm:mb-10 mb-16">
+          <h1 className=" xl:text-5xl md:text-3xl text-xl text-center text-gray-800 font-extrabold mb-5 pt-4">
+            Our Partnerships with <br />
+            Coveted Brands
+          </h1>
+          <p className="text-base md:text-lg lg:text-xl text-center text-gray-600 font-normal xl:w-10/12 xl:mx-auto">
+            At Mini Product Hunt, we take pride in collaborating with some of
+            the most innovative and sought-after brands in the tech industry.
+            Our partnerships allow us to bring cutting-edge products to our
+            community, ensuring users discover high-quality, trend-setting
+            innovations.
+          </p>
+        </div>
+        <div className="xl:py-16 lg:py-16 md:py-16 sm:py-16 px-15 flex flex-wrap">
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b lg:border-b xl:border-r lg:border-r :border-r border-gray-200 xl:pb-10 pb-16 items-center">
+            <img src="https://cdn.tuk.dev/assets/adidas-dark.png" alt />
+          </div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b lg:border-b xl:border-r lg:border-r border-gray-200 xl:pb-10 pb-16 items-center">
+            <img src="https://cdn.tuk.dev/assets/channel-dark.png" alt />
+          </div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-b lg:border-b border-gray-200 xl:pb-10 pb-16 pt-4 items-center">
+            <img src="https://cdn.tuk.dev/assets/nike-dark.png" alt />
+          </div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center lg:border-b xl:border-b lg:border-l xl:border-l border-gray-200 xl:pb-10 pb-16 items-center">
+            <img src="https://cdn.tuk.dev/assets/toyota-dark.png" alt />
+          </div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-r lg:border-r border-gray-200 xl:pt-10 items-center">
+            <img src="https://cdn.tuk.dev/assets/gs1-dark.png" alt />
+          </div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-r lg:border-r border-gray-200 xl:pt-10 items-center">
+            <img src="https://cdn.tuk.dev/assets/berry-dark.png" alt />
+          </div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:pt-10 lg:pt-10 md:pt-2 pt-16">
+            <img src="https://cdn.tuk.dev/assets/min-dark.png" alt />
+          </div>
+          <div className="w-6/12 xl:w-1/4 lg:w-1/4 md:w-1/4 flex justify-center xl:border-l lg:border-l border-gray-200 xl:pt-10 lg:pt-10 md:pt-2 pt-16">
+            <img src="https://cdn.tuk.dev/assets/honda-dark.png" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Partners;
