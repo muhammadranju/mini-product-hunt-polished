f98f54975d204d08864f4e3754f7342caecfa8bf
Muhammad Ranju <mdranju23@gmail.com>
2025-02-18 10:08:38 +0600

    Remove unused "Read more" section from Blogs component

--- src/pages/Blogs/Blogs.jsx
+++ src/pages/Blogs/Blogs.jsx
@@ -31,34 +31,7 @@ const Blogs = () => {
                     the largest marketplace of UI elements. Get inspired by 50+
                     pro design engineers. Publish your design engineering work
                   </p>
-                  <div className="w-full mt-4 justify-end flex items-center cursor-pointer">
-                    <p className="text-base tracking-wide text-indigo-500">
-                      Read more
-                    </p>
-                    <svg
-                      className="ml-3 lg:ml-6"
-                      xmlns="http://www.w3.org/2000/svg"
-                      width={20}
-                      height={18}
-                      viewBox="0 0 20 18"
-                      fill="none"
-                    >
-                      <path
-                        d="M11.7998 1L18.9998 8.53662L11.7998 16.0732"
-                        stroke="#4338ca"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                      <path
-                        d="M1 8.53662H19"
-                        stroke="#4338ca"
-                        strokeWidth={2}
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                      />
-                    </svg>
-                  </div>
+
                   <div className="h-5 w-2" />
                 </div>
               </div>
