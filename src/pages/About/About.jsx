0204f7603ac2b2f038571cdebc0229bb1d636165
Muhammad Ranju <mdranju23@gmail.com>
2025-02-18 10:09:09 +0600

    Add About page component to introduce the platform and its mission

---
+++ src/pages/About/About.jsx
@@ -0,0 +1,112 @@
+const About = () => {
+  return (
+    <div className="2xl:container 2xl:mx-auto lg:py-16 lg:px-20 md:py-12 md:px-6 py-9 px-4">
+      <div className="flex flex-col lg:flex-row justify-between gap-8">
+        <div className="w-full lg:w-5/12 flex flex-col justify-center">
+          <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">
+            About Us
+          </h1>
+          <p className="font-normal text-base leading-6 text-gray-600 ">
+            Welcome to Mini Product Hunt, the ultimate platform for discovering
+            and showcasing innovative products. Whether you're a maker launching
+            your latest creation or an enthusiast looking for the next big
+            thing, we bring together a community of innovators, creators, and
+            early adopters. Our mission is to connect groundbreaking ideas with
+            the right audience, making it easier for great products to gain
+            visibility and success.
+          </p>
+        </div>
+        <div className="w-full lg:w-8/12 ">
+          <img
+            className="w-full h-full"
+            src="https://i.ibb.co/FhgPJt8/Rectangle-116.png"
+            alt="A group of People"
+          />
+        </div>
+      </div>
+
+      <div className="flex lg:flex-row flex-col justify-between gap-8 pt-12">
+        <div className="w-full lg:w-5/12 flex flex-col justify-center">
+          <h1 className="text-3xl lg:text-4xl font-bold leading-9 text-gray-800 pb-4">
+            Our Story
+          </h1>
+          <p className="font-normal text-base leading-6 text-gray-600 ">
+            Mini Product Hunt was born out of a passion for innovation and
+            technology. Inspired by the need for a dedicated space where makers
+            and users can interact, we set out to create a platform that
+            celebrates creativity and entrepreneurship. Since our launch, we've
+            helped countless products gain traction and receive the recognition
+            they deserve. Our journey is just beginning, and we’re excited to
+            continue shaping the future of product discovery.
+          </p>
+        </div>
+        <div className="w-full lg:w-8/12 lg:pt-8">
+          <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 lg:gap-4 shadow-lg rounded-md">
+            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+              <img
+                className="md:block hidden"
+                src="https://i.ibb.co/FYTKDG6/Rectangle-118-2.png"
+                alt="Alexa featured Img"
+              />
+              <img
+                className="md:hidden block"
+                src="https://i.ibb.co/zHjXqg4/Rectangle-118.png"
+                alt="Alexa featured Img"
+              />
+              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">
+                Alexa
+              </p>
+            </div>
+            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+              <img
+                className="md:block hidden"
+                src="https://i.ibb.co/fGmxhVy/Rectangle-119.png"
+                alt="Olivia featured Img"
+              />
+              <img
+                className="md:hidden block"
+                src="https://i.ibb.co/NrWKJ1M/Rectangle-119.png"
+                alt="Olivia featured Img"
+              />
+              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">
+                Olivia
+              </p>
+            </div>
+            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+              <img
+                className="md:block hidden"
+                src="https://i.ibb.co/Pc6XVVC/Rectangle-120.png"
+                alt="Liam featued Img"
+              />
+              <img
+                className="md:hidden block"
+                src="https://i.ibb.co/C5MMBcs/Rectangle-120.png"
+                alt="Liam featued Img"
+              />
+              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">
+                Liam
+              </p>
+            </div>
+            <div className="p-4 pb-6 flex justify-center flex-col items-center">
+              <img
+                className="md:block hidden"
+                src="https://i.ibb.co/7nSJPXQ/Rectangle-121.png"
+                alt="Elijah featured img"
+              />
+              <img
+                className="md:hidden block"
+                src="https://i.ibb.co/ThZBWxH/Rectangle-121.png"
+                alt="Elijah featured img"
+              />
+              <p className="font-medium text-xl leading-5 text-gray-800 mt-4">
+                Elijah
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
