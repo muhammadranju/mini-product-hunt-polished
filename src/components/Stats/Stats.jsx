bf2d0f85b0d4160f1958cff0f196b2428a889293
Muhammad Ranju <mdranju23@gmail.com>
2025-02-18 10:08:44 +0600

    Update Stats component headings and metrics for clarity and relevance

--- src/components/Stats/Stats.jsx
+++ src/components/Stats/Stats.jsx
@@ -3,9 +3,9 @@ export default function Stats() {
     <>
       <div className="xl:px-20 px-6 py-20 xl:mx-auto xl:container">
         <h1 className="xl:text-5xl md:text-4xl text-2xl font-semibold leading-tight text-center text-gray-800 sm:mb-0 mb-12">
-          More Than 10 Years We Provide Service{" "}
+          More than 2 Years of Empowering
           <br className="md:block hidden" />
-          in Real State Industry
+          Tech Innovation
         </h1>
         <div className="md:mt-14 mt-4 relative sm:flex items-center justify-center">
           <img
@@ -27,13 +27,13 @@ export default function Stats() {
           <div className="shadow-lg xl:p-6 p-4 w-48 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 xl:mt-80 sm:mt-56 xl:-ml-0 sm:-ml-12">
             <p className="text-3xl font-semibold text-gray-800">8K+</p>
             <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">
-              Active Listening
+              Active Users
             </p>
           </div>
           <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 md:mt-0 sm:-mt-5 mt-4 right-0 xl:mr-56 sm:mr-24">
             <p className="text-3xl font-semibold text-gray-800">15K+</p>
             <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">
-              Recently Sold Lands
+              Recently Added Products
             </p>
           </div>
         </div>
